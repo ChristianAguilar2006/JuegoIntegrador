d29f6f251fff1fb2b111a1efc5ee1e29ea692ac9
globaltechss <caguilar@globaltech.com.ec>
2026-01-18 21:52:40 -0500

    deploy flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "340299863"
+    serviceWorkerVersion: "290749034"
   }
 });
